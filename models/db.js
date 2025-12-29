bd58a5898d4b9a6a1685f2cee1baa4e69d5a5dba
tahabinaziz <thzz9574@gmail.com>
2025-12-29 14:33:01 +0100

    register user

--- models/db.js
+++ models/db.js
@@ -2,6 +2,7 @@ import postgres from "postgres";
 
 export const sql = postgres({
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
