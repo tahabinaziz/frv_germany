61680302ec7c44ec2e509242b64b07d36d932cdf
tahabinaziz <thzz9574@gmail.com>
2025-12-29 15:59:01 +0100

    register user

--- models/db.js
+++ models/db.js
@@ -7,5 +7,6 @@ const sql = postgres({
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   debug: true,
+  ssl: "require",
 }); // will use psql environment variables
 export default sql;
