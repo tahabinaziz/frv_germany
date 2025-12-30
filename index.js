ade7d13eb2e7d2209f559da13d90d903705994bb
tahabinaziz <thzz9574@gmail.com>
2025-12-30 16:21:45 +0100

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUser, registerUser, resetPassword } from "./models/users.js";
 import authenticationMiddleware from "./middleware/authentication.js";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
