1636e6a5058b9d42c3d79a0fea8cc685b0203d74
tahabinaziz <thzz9574@gmail.com>
2025-12-31 00:54:33 +0100

    update register api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello from Bun + Express 🚀");
 });
 
-app.post("/users", async (req, res) => {
+app.post("/registration", async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = await getUser(email);
