8ee37319d92c83c3684bb9ecdde67fe3cc6037e2
tahabinaziz <thzz9574@gmail.com>
2026-01-01 19:03:13 +0100

    update field

--- models/applications.js
+++ models/applications.js
@@ -35,9 +35,9 @@ export async function registerApplication(
 ) {
   const newApplication = await sql`
     INSERT INTO applications 
-      (reference_number, visa_type, language,relationship_type, consulate, abh_offices, abh_document_submitted_date, case_type, status, frv_email_sent_date, appointment_conformation_date, visa_appointment_date, visa_issued_date, visa_start_date, duration_months, insurance_submitted_date, passport_collected_date, created_at)
+      (reference_number, relationship_type, consulate, abh_offices, case_type, status, frv_email_sent_date, appointment_conformation_date, visa_appointment_date, visa_issued_date, visa_start_date, duration_months, insurance_submitted_date, passport_collected_date, created_at,visa_type, language,abh_document_submitted_date)
     VALUES 
-      (${referenceNumber}, ${visa_type}, ${language}, ${relationship_type}, ${consulate}, ${abh_offices}, ${abh_document_submitted_date},${case_type}, ${status}, ${frv_email_sent_date}, ${appointment_conformation_date}, ${visa_appointment_date}, ${visa_issued_date}, ${visa_start_date}, ${duration_months}, ${insurance_submitted_date}, ${passport_collected_date}, NOW())
+      (${referenceNumber}, ${relationship_type}, ${consulate}, ${abh_offices},${case_type}, ${status}, ${frv_email_sent_date}, ${appointment_conformation_date}, ${visa_appointment_date}, ${visa_issued_date}, ${visa_start_date}, ${duration_months}, ${insurance_submitted_date}, ${passport_collected_date}, NOW(), ${visa_type}, ${language},${abh_document_submitted_date})
     RETURNING id
   `;
 
