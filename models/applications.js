6b970765a30d9d93e3b1311fd5598a56bd478fbf
tahabinaziz <thzz9574@gmail.com>
2026-01-02 20:47:52 +0100

    fix get application by user

--- models/applications.js
+++ models/applications.js
@@ -45,49 +45,29 @@ export async function registerApplication(
   return newApplication[0];
 }
 
-export async function updateApplication(
-  id,
-  reference_number,
-  visa_type,
-  language,
-  abh_document_submitted_date,
-  relationship_type,
-  consulate,
-  abh_offices,
-  case_type,
-  status,
-  frv_email_sent_date,
-  appointment_conformation_date,
-  visa_appointment_date,
-  visa_issued_date,
-  visa_start_date,
-  duration_months,
-  insurance_submitted_date,
-  passport_collected_date
-) {
+export async function updateApplication(id, data) {
   const updatedApplication = await sql`
     UPDATE applications SET
-      reference_number = ${reference_number},
-        visa_type = ${visa_type},
-       language = ${language},
-      relationship_type = ${relationship_type},
-      consulate = ${consulate},
-      abh_offices = ${abh_offices},
-        abh_document_submitted_date = ${abh_document_submitted_date},
-      case_type = ${case_type},
-      status = ${status},
-      frv_email_sent_date = ${frv_email_sent_date},
-      appointment_conformation_date = ${appointment_conformation_date},
-      visa_appointment_date = ${visa_appointment_date},
-      visa_issued_date = ${visa_issued_date},
-      visa_start_date = ${visa_start_date},
-      duration_months = ${duration_months},
-      insurance_submitted_date = ${insurance_submitted_date},
-      passport_collected_date = ${passport_collected_date}
+      reference_number = ${data.reference_number},
+      case_type = ${data.case_type},
+      language = ${data.language},
+      relationship_type = ${data.relationship_type},
+      consulate = ${data.consulate},
+      abh_offices = ${data.abh_offices},
+      abh_document_submitted_date = ${data.abh_document_submitted_date},
+      case_type = ${data.case_type},
+      status = ${data.status},
+      frv_email_sent_date = ${data.frv_email_sent_date},
+      appointment_conformation_date = ${data.appointment_conformation_date},
+      visa_appointment_date = ${data.visa_appointment_date},
+      visa_issued_date = ${data.visa_issued_date},
+      visa_start_date = ${data.visa_start_date},
+      duration_months = ${data.duration_months},
+      insurance_submitted_date = ${data.insurance_submitted_date},
+      passport_collected_date = ${data.passport_collected_date}
     WHERE id = ${id}
     RETURNING *
   `;
-
   return updatedApplication[0];
 }
 
