ab35391c120bcdbcb04e4d21c1d630b019a3c5b3
tahabinaziz <thzz9574@gmail.com>
2026-01-06 19:57:33 +0100

    new build 14

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const buildPath = path.join(__dirname, "dist/"); // Adjust if build is elsewhere
 console.log("buildPath: ", buildPath);
+
 // ----------------- STATIC FILES -----------------
 app.use(express.static(buildPath));
 
