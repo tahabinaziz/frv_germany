27611a2814a5ee2505a788ebe580e31707f6bbb3
tahabinaziz <thzz9574@gmail.com>
2026-01-06 15:33:49 +0100

    url client

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Serve React frontend
-const buildPath = path.join(__dirname, "../frontend/dist"); // Adjust if build is elsewhere
+const buildPath = path.join(__dirname, "./dist"); // Adjust if build is elsewhere
 app.use(express.static(buildPath));
 
 app.use("/api", authorizationMiddleware, router);
 // Handle React routing, return index.html for any unknown route
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 // app.get("/", (req, res) => {
