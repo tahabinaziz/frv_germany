f842834b34cc0d2f9901a4d23ca4961478933a82
tahabinaziz <thzz9574@gmail.com>
2026-01-06 19:36:03 +0100

    new build 10

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log("__dirname", __dirname);
+
 // Serve React frontend
 const buildPath = path.join(__dirname, "dist"); // Adjust if build is elsewhere
 // ----------------- STATIC FILES -----------------
